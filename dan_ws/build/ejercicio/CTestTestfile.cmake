035c147c5310c1c7ce702a2889925aa471f4d52e
Daniel-hp <danielhp@ciencias.unam.mx>
2017-10-17 10:22:57 -0500

    Se hacen cosas raras porque nada funciona

--- dan_ws/build/ejercicio/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dan/dan_ws/src/ejercicio
-# Build directory: /home/dan/dan_ws/build/ejercicio
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
