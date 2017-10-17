d065e9e783fba1dd4d63dd0fb63f9d9997d38c76
Daniel-hp <danielhp@ciencias.unam.mx>
2017-10-17 10:24:38 -0500

    Se vuelven a agregar los archivos

---
+++ dan_ws/devel/share/common-lisp/ros/beginner_tutorials/msg/Num.lisp
@@ -0,0 +1,76 @@
+; Auto-generated. Do not edit!
+
+
+(cl:in-package beginner_tutorials-msg)
+
+
+;//! \htmlinclude Num.msg.html
+
+(cl:defclass <Num> (roslisp-msg-protocol:ros-message)
+  ((Amr
+    :reader Amr
+    :initarg :Amr
+    :type cl:string
+    :initform ""))
+)
+
+(cl:defclass Num (<Num>)
+  ())
+
+(cl:defmethod cl:initialize-instance :after ((m <Num>) cl:&rest args)
+  (cl:declare (cl:ignorable args))
+  (cl:unless (cl:typep m 'Num)
+    (roslisp-msg-protocol:msg-deprecation-warning "using old message class name beginner_tutorials-msg:<Num> is deprecated: use beginner_tutorials-msg:Num instead.")))
+
+(cl:ensure-generic-function 'Amr-val :lambda-list '(m))
+(cl:defmethod Amr-val ((m <Num>))
+  (roslisp-msg-protocol:msg-deprecation-warning "Using old-style slot reader beginner_tutorials-msg:Amr-val is deprecated.  Use beginner_tutorials-msg:Amr instead.")
+  (Amr m))
+(cl:defmethod roslisp-msg-protocol:serialize ((msg <Num>) ostream)
+  "Serializes a message object of type '<Num>"
+  (cl:let ((__ros_str_len (cl:length (cl:slot-value msg 'Amr))))
+    (cl:write-byte (cl:ldb (cl:byte 8 0) __ros_str_len) ostream)
+    (cl:write-byte (cl:ldb (cl:byte 8 8) __ros_str_len) ostream)
+    (cl:write-byte (cl:ldb (cl:byte 8 16) __ros_str_len) ostream)
+    (cl:write-byte (cl:ldb (cl:byte 8 24) __ros_str_len) ostream))
+  (cl:map cl:nil #'(cl:lambda (c) (cl:write-byte (cl:char-code c) ostream)) (cl:slot-value msg 'Amr))
+)
+(cl:defmethod roslisp-msg-protocol:deserialize ((msg <Num>) istream)
+  "Deserializes a message object of type '<Num>"
+    (cl:let ((__ros_str_len 0))
+      (cl:setf (cl:ldb (cl:byte 8 0) __ros_str_len) (cl:read-byte istream))
+      (cl:setf (cl:ldb (cl:byte 8 8) __ros_str_len) (cl:read-byte istream))
+      (cl:setf (cl:ldb (cl:byte 8 16) __ros_str_len) (cl:read-byte istream))
+      (cl:setf (cl:ldb (cl:byte 8 24) __ros_str_len) (cl:read-byte istream))
+      (cl:setf (cl:slot-value msg 'Amr) (cl:make-string __ros_str_len))
+      (cl:dotimes (__ros_str_idx __ros_str_len msg)
+        (cl:setf (cl:char (cl:slot-value msg 'Amr) __ros_str_idx) (cl:code-char (cl:read-byte istream)))))
+  msg
+)
+(cl:defmethod roslisp-msg-protocol:ros-datatype ((msg (cl:eql '<Num>)))
+  "Returns string type for a message object of type '<Num>"
+  "beginner_tutorials/Num")
+(cl:defmethod roslisp-msg-protocol:ros-datatype ((msg (cl:eql 'Num)))
+  "Returns string type for a message object of type 'Num"
+  "beginner_tutorials/Num")
+(cl:defmethod roslisp-msg-protocol:md5sum ((type (cl:eql '<Num>)))
+  "Returns md5sum for a message object of type '<Num>"
+  "41627bb8959d7ae85c352700984f35cc")
+(cl:defmethod roslisp-msg-protocol:md5sum ((type (cl:eql 'Num)))
+  "Returns md5sum for a message object of type 'Num"
+  "41627bb8959d7ae85c352700984f35cc")
+(cl:defmethod roslisp-msg-protocol:message-definition ((type (cl:eql '<Num>)))
+  "Returns full string definition for message of type '<Num>"
+  (cl:format cl:nil "string Amr~%~%~%"))
+(cl:defmethod roslisp-msg-protocol:message-definition ((type (cl:eql 'Num)))
+  "Returns full string definition for message of type 'Num"
+  (cl:format cl:nil "string Amr~%~%~%"))
+(cl:defmethod roslisp-msg-protocol:serialization-length ((msg <Num>))
+  (cl:+ 0
+     4 (cl:length (cl:slot-value msg 'Amr))
+))
+(cl:defmethod roslisp-msg-protocol:ros-message-to-list ((msg <Num>))
+  "Converts a ROS message object to a list"
+  (cl:list 'Num
+    (cl:cons ':Amr (Amr msg))
+))
